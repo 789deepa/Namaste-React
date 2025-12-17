3880d3a95b51c28c3031264ba2c7593ed9b3f873
Deepali Patil <dspatil371123@gmail.com>
2025-12-17 22:16:08 +0530

    new components

--- app.js
+++ app.js
@@ -1,17 +1,75 @@
-// EP 01 - Creating React Elements without JSX
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = 
-    React.createElement("div", {id: "parent"}, 
-    [React.createElement("div", {id: "child"},
-    React.createElement("h1", {}, "Hellow"),
-    React.createElement("h1", {}, "Meouu"))],
 
-    [React.createElement("div", {id: "child2"},
-    React.createElement("h1", {}, "Wowww"))]
-)
+/* 
+* Header 
+* -logo
+* -Nav items
+* Body
+* -Search
+* -RestaurantContainer
+*   - Restaurant cards
+*     -img -name - rating 
+* Footer
+* -copyright
+* -contact 
+*/
 
-// const heading = React.createElement("h1", {id:"heading"}, "Welcome to React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-// console.log(heading)
+const Header = () => {
+    return (
+        <div className = "header">
+            <div className = "logo-container">
+                <img alt="Logo" className="logo" src="/img/logo.png" />
 
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const RestoCard = (_props) => {
+    return (
+        <div className="res-cards">
+            <img className="res-card" alt="Puran Poli" src="/img/food.jpg" />
+            <h3>{_props.resName}</h3>  // Dynamic Data from Props
+            <h4> Puran Poli, Maharashtrian Food </h4>
+            <h4>4.5 Stars</h4>
+            <h4>38 Min </h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {/* resto cards */}
+                <RestoCard resName="MOONLIGHT" />
+                <RestoCard resName="KFC" />
+               
+            </div>
+        </div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body />
+        </div> 
+               
+    );    
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<AppLayout />);
--- notes.md
+++ notes.md
@@ -9,4 +9,27 @@ There are two types of dependancies
 ^ (caret) - update minor + patch 18.x.x 
 ~ (tilde) - update patch only 18.2.x
 
+<<<<<<< HEAD
 'Transitive Dependancy' - Parcel dependancy can have its own dependancy, those can hav their own dependancies. (parcel == beast)
+=======
+'Transitive Dependancy' - Parcel dependancy can have its own dependancy, those can hav their own dependancies. (parcel == beast)
+
+whatever we can regenrate- dont have to put it on git (Ex. Node_modules)
+
+npm - calling command
+npx - executing packadge (npx parcel ___ )
+
+parcel: 
+-Dev build
+-Local Server
+-HMR = hot module replacement
+-File watching algorithm - written in cpp
+-Caching - faster builds
+-Image optimization 
+-Minification of files, for production 
+-Consistent Hashing
+-Code splitting
+-Differential bundling
+(big round of appolause for Parcel )
+-when we do 'npx parcel build index.html(automatically generates app.js)', have to remove main = app.js from packadge.json file (otherwise error)
+>>>>>>> bed229b (Added basic components)
